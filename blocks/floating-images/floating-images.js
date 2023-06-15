558d115ab8125f6351e9af2dcc59936ca4886f02
dan <dan.collins@gmail.com>
2023-06-15 08:05:58 -0400

    more positioning, heights flexes.....

--- blocks/floating-images/floating-images.js
+++ blocks/floating-images/floating-images.js
@@ -5,4 +5,6 @@ export default function decorate(block) {
   const container = block.querySelector(':scope > div');
   container.children[0].classList.add('content');
   container.children[1].classList.add('image');
+  /*** localhost images are broken so adding myself for testing to be removed when there is a fix***/
+  /*container.children[1].innerHTML = '<img src="https://floating-image-issue-10--takeda-ihs--hlxsites.hlx.page/drafts/media_10701ea634856c238f084328f0cfb6c43894a1511.png?width=2000&format=webply&optimize=medium">';*/
 }
