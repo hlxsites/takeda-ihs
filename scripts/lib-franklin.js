7b66aa9c54afd58a78081c6cd88db344fe0c0f6a
dan <dan.collins@gmail.com>
2023-06-16 10:45:55 -0400

    lots of display layer changes to match IHS style.....

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -129,6 +129,7 @@ export async function loadScript(src, attrs) {
  * @returns {string} The metadata value(s)
  */
 export function getMetadata(name) {
+  console.log(name);
   const attr = name && name.includes(':') ? 'property' : 'name';
   const meta = [...document.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
   return meta || '';
