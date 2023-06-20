a86db1076ed9e5b62a7c4bc8196f90efa6f2bd4d
dan <dan.collins@gmail.com>
2023-06-20 09:45:45 -0400

    # This is a combination of 10 commits. # This is the 1st commit message:
    more lint changes and padding/margin for image and image-wrapper
    
    # The commit message #2 will be skipped:
    
    # two more lint issues
    
    # The commit message #3 will be skipped:
    
    # lint-I dont think I will ever get this right
    
    # The commit message #4 will be skipped:
    
    # lint
    
    # The commit message #5 will be skipped:
    
    # linting again
    
    # The commit message #6 will be skipped:
    
    # linting spaces
    
    # The commit message #7 will be skipped:
    
    # linting take 100000
    
    # The commit message #8 will be skipped:
    
    # linting take again
    
    # The commit message #9 will be skipped:
    
    # 3 more errors
    
    # The commit message #10 will be skipped:
    
    # 4 bad lints

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,15 +11,14 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
-  getMetadata,
 } from './lib-franklin.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
- */
+* Builds hero block and prepends to main in a new section.
+* @param {Element} main The container element
+*/
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
@@ -32,17 +31,17 @@ function buildHeroBlock(main) {
 }
 
 /**
- * Build Floating image block
- * @param {Element} main The container element
- */
- function buildFloatingImages(main) {
-   main.querySelectorAll('.section-metadata').forEach((metadata) => {
+* Builds floating image block and prepends to main in a new section.
+* @param {Element} main The container element
+*/
+function buildFloatingImages(main) {
+  main.querySelectorAll('.section-metadata').forEach((metadata) => {
     let style;
     [...metadata.querySelectorAll(':scope > div')].every((div) => {
       const match = div.children[1]?.textContent.toLowerCase().trim().match(/(image-(left|right))/);
       if (div.children[0]?.textContent.toLowerCase().trim() === 'style' && match) {
-        [, style] = match;
-        return false;
+       [, style] = match;
+         return false;
       }
       return true;
     });
