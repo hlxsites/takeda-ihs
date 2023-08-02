e131e224d54ea35f6dde8d04f0858442c7a0c9f0
Teriyana Cohens <teriyana.cohens@slalom.com>
2023-08-02 18:03:33 -0400

    updated based on feedback

--- blocks/disclaimer-modal/disclaimer-modal.js
+++ blocks/disclaimer-modal/disclaimer-modal.js
@@ -1,29 +1,40 @@
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
+
 export default async function decorate(block) {
-const modalContainer = document.querySelector('.disclaimer-modal-container');
-const modal = document.querySelector('.disclaimer-modal');
-modal.children[0].classList.add('modal-title');
-const modalTitle = document.querySelector('.modal-title');
-modalTitle.nextElementSibling.classList.add('modal-body-content');
-const modalBodyText = document.querySelector('.modal-body-content');
-modalBodyText.nextElementSibling.classList.add('modal-button-wrapper');
-const modalButtonWrapper = document.querySelector('.modal-button-wrapper');
-modalButtonWrapper.children[0].classList.add('modal-agree-button');
-modalButtonWrapper.children[1].classList.add('modal-go-button');
-const acceptButn = document.querySelector('.modal-agree-button');
-hcpDisclaimerCheck(document, modalContainer, acceptButn);
-
-  }
-  async function hcpDisclaimerCheck(document, modal, acceptButn) {
-    const isModalAccepted = document.cookie.match(/\shcpModalDismiss=1;?/) !== null;
-    const shouldShowModal = !isModalAccepted;
+  const response = await fetch(`/drafts/teriyana/disclaimer-modal.plain.html`);
+  const isModalAccepted = document.cookie.match(/\shcpModalDismiss=1;?/) !== null || window.location.href.indexOf("?bypassModal") > -1;
+  const shouldShowModal = !isModalAccepted || (document.location.href.indexOf("?showModal") > -1);
+  const modalContainer = block.parentElement.parentElement;
   
-    if (shouldShowModal) {
-        modal.style.display = 'block';
-        acceptButn.onclick = function(){
+  if (response.ok && shouldShowModal) {
+    modalContainer.style.display = 'block';
+    const html = await response.text();
+    const tmp = document.createElement('div');
+    tmp.innerHTML = html;
+    const modal = tmp.querySelector('.disclaimer-modal');
+    const config = readBlockConfig(modal);
+    block.innerHTML = `
+  <div class="title"><h2>${config.title}</h2></div>
+  <div class="content"><p> ${config.content}</p></div>
+  <div class="button-section">
+  <div class="agree"><p> ${config.agree}</p></div>
+  <div class="leave"><a class="link" href=" ${config.link} "> <p>${config.leave} </p></a></div>
+  </div>
+  `
+    const acceptButn = block.querySelector('.agree');
+    acceptButn.addEventListener('click', function () {
+      var CookieDate = new Date;
+      CookieDate.setFullYear(CookieDate.getFullYear() +5);
+      document.cookie = `hcpModalDismiss=1;path=/;myCookie=to_be_deleted;expires=${CookieDate.toUTCString()};`;
+      modalContainer.style.display = 'none';
+    });
+   
 
-            document.cookie += 'hcpModalDismiss=1';
-            modal.style.display = 'none';
-        }
-      
-    }
   }
+  else {
+    block.parentElement.style.background = 'none';
+    block.parentElement.parentElement.style.background = 'none';
+    block.style.display = 'none';
+
+  }
+}
