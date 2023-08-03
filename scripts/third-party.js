36a563a836c9577761b44c57b21dfbf4800ce9c6
Bryan Stopp <bryan.stopp@gmail.com>
2023-08-03 08:58:55 -0500

    Add GTM library.

---
+++ scripts/third-party.js
@@ -0,0 +1,17 @@
+const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P49296M');`;
+
+function createInlineScript(innerHTML, parent) {
+  const script = document.createElement('script');
+  script.type = 'text/partytown';
+  script.innerHTML = innerHTML;
+  parent.appendChild(script);
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export function integrateMartech() {
+  createInlineScript(GTM_SCRIPT, document.body);
+}
