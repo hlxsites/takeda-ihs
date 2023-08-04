648e17560cf18320b222dc5c469b9540bf8cc536
Teriyana Cohens <teriyana.cohens@slalom.com>
2023-08-04 13:01:01 -0400

    fixed linting issues

--- blocks/disclaimer-modal/disclaimer-modal.js
+++ blocks/disclaimer-modal/disclaimer-modal.js
@@ -1,11 +1,10 @@
 import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
-  const response = await fetch(`/drafts/teriyana/disclaimer-modal.plain.html`);
-  const isModalAccepted = document.cookie.match(/\shcpModalDismiss=1;?/) !== null || window.location.href.indexOf("?bypassModal") > -1;
-  const shouldShowModal = !isModalAccepted || (document.location.href.indexOf("?showModal") > -1);
+  const response = await fetch('/drafts/teriyana/disclaimer-modal.plain.html');
+  const isModalAccepted = document.cookie.match(/\shcpModalDismiss=1;?/) !== null || window.location.href.indexOf('?bypassModal') > -1;
+  const shouldShowModal = !isModalAccepted || (document.location.href.indexOf('?showModal') > -1);
   const modalContainer = block.parentElement.parentElement;
-  
   if (response.ok && shouldShowModal) {
     modalContainer.style.display = 'block';
     const html = await response.text();
@@ -20,16 +19,14 @@ export default async function decorate(block) {
   <div class="agree"><p> ${config.agree}</p></div>
   <div class="leave"><a class="link" href=" ${config.link} "> <p>${config.leave} </p></a></div>
   </div>
-  `
+  `;
     const acceptButn = block.querySelector('.agree');
     acceptButn.addEventListener('click', function () {
-      var CookieDate = new Date;
-      CookieDate.setFullYear(CookieDate.getFullYear() +5);
+      const CookieDate = new Date();
+      CookieDate.setFullYear(CookieDate.getFullYear() + 5);
       document.cookie = `hcpModalDismiss=1;path=/;myCookie=to_be_deleted;expires=${CookieDate.toUTCString()};`;
       modalContainer.style.display = 'none';
     });
-   
-
   }
   else {
     block.parentElement.style.background = 'none';
