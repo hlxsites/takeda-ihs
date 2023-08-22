65f2c254809eae2a340c1e433ee6e808f86bb4fc
Lance Wintch <lwintch@users.noreply.github.com>
2023-08-22 13:33:49 -0600

    Revert "Add OneTrust call. (#125)" (#143)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,16 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
 
-function addCookieOneTrust() {
-  const cookieScript = document.createElement('script');
-  cookieScript.src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
-  cookieScript.type = 'text/javascript';
-  cookieScript.setAttribute('data-domain-script', 'ae0babd7-7067-4af4-bfa9-84ae35b5957f');
-  document.head.appendChild(cookieScript);
-}
-
-addCookieOneTrust();
-
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
