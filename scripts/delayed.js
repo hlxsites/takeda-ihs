5dabf2d66df759ae74302126436f8f53e16a7bdb
dan <dan.collins@vitals.com>
2024-01-28 20:27:17 -0500

    added adoibe launch library for testing

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,13 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { sampleRUM, loadScript } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+const loadAdobeDTM = async () => {
+  await loadScript('https://assets.adobedtm.com/8fee56b0a165/a42c27096959/launch-c6342f10fc46-development.min.js');
+};
+
+await loadAdobeDTM();
