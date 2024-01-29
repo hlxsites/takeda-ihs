879b1473f8b54941250ceb41150756265f08277b
dan <dan.collins@vitals.com>
2024-01-29 13:03:04 -0500

    changing launch link
    development seems to work

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,7 +7,7 @@ sampleRUM('cwv');
 // add more delayed functionality here
 
 const loadAdobeDTM = async () => {
-  await loadScript('https://assets.adobedtm.com/8fee56b0a165/a42c27096959/launch-c6342f10fc46-development.min.js');
+  await loadScript('https://assets.adobedtm.com/8fee56b0a165/4f6b3b702502/launch-9b9e958a7168-development.min.js');
 };
 
 await loadAdobeDTM();
