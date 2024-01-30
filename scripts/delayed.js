70eaf4db12669e373c632170e9bbcbdc7f952c4b
dan <dan.collins@vitals.com>
2024-01-30 12:39:00 -0500

    adding launch link for new environment

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,7 +7,10 @@ sampleRUM('cwv');
 // add more delayed functionality here
 
 const loadAdobeDTM = async () => {
-  await loadScript('https://assets.adobedtm.com/8fee56b0a165/4f6b3b702502/launch-9b9e958a7168-development.min.js');
+  await loadScript('https://assets.adobedtm.com/8fee56b0a165/0dcea4176083/launch-868c1997ca51-development.min.js');
 };
 
 await loadAdobeDTM();
+
+
+<script src="https://assets.adobedtm.com/8fee56b0a165/0dcea4176083/launch-868c1997ca51-development.min.js" async></script>
