f8e48cbcbd65be9b77d240537ad237e3baaa8146
dan <dan.collins@vitals.com>
2024-02-09 06:28:44 -0500

    remove gtm

--- scripts/third-party.js
+++ scripts/third-party.js
@@ -1,8 +1,8 @@
-//* const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//* new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//* j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//* 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//* })(window,document,'script','dataLayer','GTM-P49296M');`; *//
+const GTM_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P49296M');`;
 
 function createInlineScript(innerHTML, parent) {
   const script = document.createElement('script');
@@ -14,6 +14,6 @@ function createInlineScript(innerHTML, parent) {
 // eslint-disable-next-line import/prefer-default-export
 export function integrateMartech() {
   if (window.location.href.match(/^https?:\/\/(localhost|127.0.0.1)/) === null) {
-    createInlineScript(GTM_SCRIPT, document.body);
+    createInlineScript(document.body);
   }
 }
