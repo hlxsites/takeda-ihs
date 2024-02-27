e203495192c3bc4b022b45cca550b9bcf577ae2b
dan <dan.collins@vitals.com>
2024-02-27 14:35:03 -0500

    redo for product cards

--- blocks/cards/product.js
+++ blocks/cards/product.js
@@ -5,10 +5,15 @@
 export default async function decorate(block) {
   [...block.children].forEach((card) => {
     card.classList.add('card');
-
+    const downloadButton = card.children[2];
     card.children[0].classList.add('image');
     card.children[1].classList.add('details');
-
+    const button = document.createElement('div');
+    button.classList.add('button-wrapper');
+    console.log(downloadButton);
+    const buttonLink = card.querySelector('p.button-container');
+    card.append(button);
+    button.append(buttonLink);
     let p = card.children[1].querySelector('p');
     if (!p) {
       p = document.createElement('p');
